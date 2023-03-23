3bf7d57ee8bb5b8462630ee8f183295828d45372
cmdctl <anton.bozhinov@gmail.com>
2023-03-24 00:18:30 +0200

    finish db exec to mssql

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,43 @@
 use anyhow::Result;
 use sql_tools::exec::exec_sql;
-use terminal_ui::repository::FsTenguRepository;
+use terminal_ui::{repository::FsTenguRepository, start_tui};
 use tokio::main;
+use clap::{Parser, Subcommand};
+use std::io;
 
 mod terminal_ui;
 mod sql_tools;
 
+#[derive(Subcommand, Debug)]
+enum Command {
+    Exec,
+}
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[command(subcommand)]
+    commands: Option<Command>,
+}
+
 #[main]
 async fn main() -> Result<()> {
-    let repo = FsTenguRepository::new();
-    let sql = "SELECT TOP 5 * FROM tbl_trade";
-    exec_sql(repo, sql).await?;
+    let args = Args::parse();
+    match args.commands {
+        Some(Command::Exec) => {
+            let mut sql = String::new();
+            let mut lines = io::stdin().lines();
+            while let Some(line) = lines.next() {
+                let line = line?;
+                if !line.starts_with("--") {
+                    sql.push_str(&line);
+                }
+            }
+            let repo = FsTenguRepository::new();
+            exec_sql(repo, &sql).await?;
+        }
+        None => {
+            start_tui()?;
+        }
+    }
     Ok(())
 }
--- src/sql_tools/exec.rs
+++ src/sql_tools/exec.rs
@@ -1,8 +1,12 @@
-use tiberius::{Client, Config, AuthMethod, Column, Row, ColumnType, xml::XmlData, time::chrono::NaiveDateTime, Uuid, numeric::Decimal};
-use tokio::net::TcpStream;
-use tokio_util::compat::{TokioAsyncWriteCompatExt, Compat};
 use crate::terminal_ui::repository::TenguRepository;
-use anyhow::{Result, anyhow};
+use anyhow::{anyhow, Result};
+use prettytable::{Cell, Row as TRow, Table};
+use tiberius::{
+    numeric::Numeric, time::chrono::NaiveDateTime, xml::XmlData, AuthMethod, Client, Column,
+    ColumnType, Config, Row, Uuid,
+};
+use tokio::net::TcpStream;
+use tokio_util::compat::{Compat, TokioAsyncWriteCompatExt};
 
 pub async fn get_conn<R: TenguRepository>(repo: R) -> Result<Client<Compat<TcpStream>>> {
     let Some(conn) = repo.get_active_connection() else {
@@ -23,25 +27,29 @@ pub async fn get_conn<R: TenguRepository>(repo: R) -> Result<Client<Compat<TcpSt
 
 pub async fn exec_sql<R: TenguRepository>(repo: R, sql: &str) -> Result<()> {
     let mut conn = get_conn(repo).await?;
+    let mut table = Table::new();
     let stream = conn.simple_query(sql).await?.into_results().await?;
     for rows in stream {
         for (i, row) in rows.iter().enumerate() {
             if i == 0 {
+                let mut headers = Vec::with_capacity(row.columns().len());
                 row.columns().iter().for_each(|col| {
-                    print!("{} ", col.name());
+                    headers.push(Cell::new(col.name()));
                 });
-                println!();
+                table.add_row(TRow::new(headers));
             }
+            let mut row_data = Vec::with_capacity(row.columns().len());
             row.columns().iter().for_each(|col| {
-                println!("{}: {:?}", col.name(), get_value(col, row));
+                row_data.push(Cell::new(&get_value(row, col)));
             });
+            table.add_row(TRow::new(row_data));
         }
-        
     }
+    table.printstd();
     Ok(())
 }
 
-fn get_value<'a>(col: &Column, row: &'a Row) -> String {
+fn get_value<'a>(row: &'a Row, col: &Column) -> String {
     match col.column_type() {
         ColumnType::Bit | ColumnType::Bitn => {
             if let Some(val) = row.get::<bool, _>(col.name()) {
@@ -72,8 +80,15 @@ fn get_value<'a>(col: &Column, row: &'a Row) -> String {
                 "NULL".to_string()
             }
         }
-        ColumnType::Int1 => {
-            if let Some(val) = row.get::<u8, _>(col.name()) {
+        ColumnType::Int1 | ColumnType::Int2 | ColumnType::Int4 | ColumnType::Intn => {
+            if let Some(val) = row.get::<i32, _>(col.name()) {
+                val.to_string()
+            } else {
+                "NULL".to_string()
+            }
+        }
+        ColumnType::Int8 => {
+            if let Some(val) = row.get::<i64, _>(col.name()) {
                 val.to_string()
             } else {
                 "NULL".to_string()
@@ -81,47 +96,26 @@ fn get_value<'a>(col: &Column, row: &'a Row) -> String {
         }
         ColumnType::BigBinary | ColumnType::BigVarBin => {
             if let Some(val) = row.get::<&'a [u8], _>(col.name()) {
-                val.iter().map(|b| format!("{:02X}", b)).collect::<Vec<String>>().join(" ")
-            } else {
-                "NULL".to_string()
-            }
-        }
-        ColumnType::Int2 => {
-            if let Some(val) = row.get::<i16, _>(col.name()) {
-                val.to_string()
-            } else {
-                "NULL".to_string()
-            }
-        }
-        ColumnType::Int4 => {
-            if let Some(val) = row.get::<i32, _>(col.name()) {
-                val.to_string()
-            } else {
-                "NULL".to_string()
-            }
-        }
-        ColumnType::Intn => {
-            if let Ok(val) = row.try_get::<u8, _>(col.name()) {
-                val.unwrap_or(0).to_string()
-            } else {
-                "NULL".to_string()
-            }
-        }
-        ColumnType::Int8 => {
-            if let Some(val) = row.get::<i32, _>(col.name()) {
-                val.to_string()
+                val.iter()
+                    .map(|b| format!("{:02X}", b))
+                    .collect::<Vec<String>>()
+                    .join(" ")
             } else {
                 "NULL".to_string()
             }
         }
         ColumnType::Decimaln | ColumnType::Numericn => {
-            if let Some(val) = row.get::<Decimal, _>(col.name()) {
+            if let Some(val) = row.get::<Numeric, _>(col.name()) {
                 val.to_string()
             } else {
                 "NULL".to_string()
             }
         }
-        ColumnType::Datetime | ColumnType::Timen | ColumnType::Datetime2 | ColumnType::DatetimeOffsetn | ColumnType::Datetime4 => {
+        ColumnType::Datetime
+        | ColumnType::Timen
+        | ColumnType::Datetime2
+        | ColumnType::DatetimeOffsetn
+        | ColumnType::Datetime4 => {
             if let Some(val) = row.get::<NaiveDateTime, _>(col.name()) {
                 val.to_string()
             } else {
@@ -144,5 +138,3 @@ fn get_value<'a>(col: &Column, row: &'a Row) -> String {
         }
     }
 }
-
-
