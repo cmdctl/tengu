5328ed5156c23069514f20f8feaad869be46089b
cmdctl <anton.bozhinov@gmail.com>
2023-04-04 11:10:51 +0300

    remove test command

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-use std::collections::HashSet;
-
 use anyhow::Result;
 use clap::{Parser, Subcommand};
-use db::{service::{TenguService, Service}, table::Table};
 use lsp::server::start_lsp;
-use terminal_ui::{start_tui, repository::{FsTenguRepository, TenguRepository}};
+use terminal_ui::start_tui;
 use tokio::main;
 
 mod db;
@@ -16,7 +13,6 @@ mod tokenizer;
 #[derive(Subcommand, Debug)]
 enum Command {
     Lsp,
-    Test,
 }
 
 #[derive(Parser, Debug)]
@@ -29,22 +25,6 @@ struct Args {
 async fn main() -> Result<()> {
     let args = Args::parse();
     match args.commands {
-        Some(Command::Test) => {
-            let repo = FsTenguRepository::new();
-            let Some(active_connection) = repo.get_active_connection() else {
-                eprintln!("No active connection found");
-                return Ok(());
-            };
-            let service = TenguService::new(active_connection.engine, repo);
-            let mut set = HashSet::new();
-            let table = Table {
-                name: "demo".to_string(),
-                schema: "public".to_string(),
-            };
-            set.insert(table.clone());
-            let col = service.get_table_columns(set).await?;
-            println!("{:?}", col);
-        }
         Some(Command::Lsp) => {
             start_lsp().await;
         }
